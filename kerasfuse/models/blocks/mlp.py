a9b7596f441c51928517c8f6fd6629319ec44b21
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-06-18 00:25:41 +0300

    feat: 🚀 mlp.py MLPMixerLayer fix and test case added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- kerasfuse/models/blocks/mlp.py
+++ kerasfuse/models/blocks/mlp.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
-from keras.layers.normalization.layer_normalization import LayerNormalization
 from tensorflow import keras
-from tensorflow.python.keras.layers import Activation, Dense, Dropout, Layer
+from tensorflow.python.keras import layers
 
 """
 Author: Khalid Salama
@@ -9,7 +8,7 @@ https://keras.io/examples/vision/mlp_image_classification/
 """
 
 
-class Patches(Layer):
+class Patches(layers.Layer):
     def __init__(self, patch_size, num_patches):
         super().__init__()
         self.patch_size = patch_size
@@ -28,53 +27,46 @@ class Patches(Layer):
         patches = tf.reshape(patches, [batch_size, self.num_patches, patch_dims])
         return patches
 
-    class MLPMixerLayer(Layer):
-        def __init__(
-            self,
-            num_patches,
-            hidden_units,
-            dropout_rate,
-            embedding_dim,
-            *args,
-            **kwargs
-        ):
-            super().__init__(*args, **kwargs)
 
-            self.mlp1 = keras.Sequential(
-                [
-                    Dense(units=num_patches),
-                    Activation(keras.activations.gelu),
-                    Dense(units=num_patches),
-                    Dropout(rate=dropout_rate),
-                ]
-            )
-            self.mlp2 = keras.Sequential(
-                [
-                    Dense(units=num_patches),
-                    Activation(keras.activations.gelu),
-                    Dense(units=embedding_dim),
-                    Dropout(rate=dropout_rate),
-                ]
-            )
-            self.normalize = LayerNormalization(epsilon=1e-6)
+class MLPMixerLayer(layers.Layer):
+    def __init__(self, num_patches, dropout_rate, embedding_dim, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        def call(self, inputs):
-            # Apply layer normalization.
-            x = self.normalize(inputs)
-            # Transpose inputs from [num_batches, num_patches,
-            # hidden_units] to [num_batches, hidden_units, num_patches].
-            x_channels = tf.linalg.matrix_transpose(x)
-            # Apply mlp1 on each channel independently.
-            mlp1_outputs = self.mlp1(x_channels)
-            # Transpose mlp1_outputs from [num_batches,
-            # hidden_dim, num_patches] to [num_batches, num_patches, hidden_units].
-            mlp1_outputs = tf.linalg.matrix_transpose(mlp1_outputs)
-            # Add skip connection.
-            x = mlp1_outputs + inputs
-            # Apply layer normalization.
-            x_patches = self.normalize(x)
-            # Apply mlp2 on each patch independtenly.
-            mlp2_outputs = self.mlp2(x_patches)
-            # Add skip connection.
-            x = x + mlp2_outputs
-            return x
+        self.mlp1 = keras.Sequential(
+            [
+                layers.Dense(units=num_patches),
+                layers.Activation(keras.activations.gelu),
+                layers.Dense(units=num_patches),
+                layers.Dropout(rate=dropout_rate),
+            ]
+        )
+        self.mlp2 = keras.Sequential(
+            [
+                layers.Dense(units=num_patches),
+                layers.Activation(keras.activations.gelu),
+                layers.Dense(units=embedding_dim),
+                layers.Dropout(rate=dropout_rate),
+            ]
+        )
+        self.normalize = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+    def call(self, inputs):
+        # Apply layer normalization.
+        x = self.normalize(inputs)
+        # Transpose inputs from [num_batches, num_patches,
+        # hidden_units] to [num_batches, hidden_units, num_patches].
+        x_channels = tf.linalg.matrix_transpose(x)
+        # Apply mlp1 on each channel independently.
+        mlp1_outputs = self.mlp1(x_channels)
+        # Transpose mlp1_outputs from [num_batches,
+        # hidden_dim, num_patches] to [num_batches, num_patches, hidden_units].
+        mlp1_outputs = tf.linalg.matrix_transpose(mlp1_outputs)
+        # Add skip connection.
+        x = mlp1_outputs + inputs
+        # Apply layer normalization.
+        x_patches = self.normalize(x)
+        # Apply mlp2 on each patch independtenly.
+        mlp2_outputs = self.mlp2(x_patches)
+        # Add skip connection.
+        x = x + mlp2_outputs
+        return x
---
+++ tests/test_mlp_mixer_layer.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from kerasfuse.models.blocks.mlp import MLPMixerLayer, Patches
+
+"""
+Test the MLP Mixer Layer
+Based on:
+Author: Khalid Salama
+https://keras.io/examples/vision/mlp_image_classification/
+"""
+
+# Test Data Preparation
+num_classes = 100
+input_shape = (32, 32, 3)
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
+
+print(f"x_train shape: {x_train.shape} - y_train shape: {y_train.shape}")
+print(f"x_test shape: {x_test.shape} - y_test shape: {y_test.shape}")
+
+weight_decay = 0.0001
+batch_size = 128
+num_epochs = 5
+dropout_rate = 0.2
+image_size = 64  # We'll resize input images to this size.
+patch_size = 8  # Size of the patches to be extracted from the input images.
+num_patches = (image_size // patch_size) ** 2  # Size of the data array.
+embedding_dim = 256  # Number of hidden units.
+num_blocks = 4  # Number of blocks.
+learning_rate = 0.005
+
+
+print(f"Image size: {image_size} X {image_size} = {image_size ** 2}")
+print(f"Patch size: {patch_size} X {patch_size} = {patch_size ** 2} ")
+print(f"Patches per image: {num_patches}")
+print(f"Elements per patch (3 channels): {(patch_size ** 2) * 3}")
+
+
+data_augmentation = keras.Sequential(
+    [
+        tf.keras.layers.Normalization(),
+        tf.keras.layers.Resizing(image_size, image_size),
+        tf.keras.layers.RandomFlip("horizontal"),
+        layers.RandomZoom(height_factor=0.2, width_factor=0.2),
+    ],
+    name="data_augmentation",
+)
+# Compute the mean and the variance of the training data for normalization.
+data_augmentation.layers[0].adapt(x_train)
+
+
+def build_classifier(blocks, positional_encoding=False):
+    inputs = layers.Input(shape=input_shape)
+    # Augment data.
+    augmented = data_augmentation(inputs)
+    # Create patches.
+    patches = Patches(patch_size, num_patches)(augmented)
+    # Encode patches to generate a [batch_size, num_patches, embedding_dim] tensor.
+    x = layers.Dense(units=embedding_dim)(patches)
+    if positional_encoding:
+        positions = tf.range(start=0, limit=num_patches, delta=1)
+        position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=embedding_dim
+        )(positions)
+        x = x + position_embedding
+    # Process x using the module blocks.
+    x = blocks(x)
+    """
+    Apply global average pooling to generate
+    """
+    representation = layers.GlobalAveragePooling1D()(x)
+    # Apply dropout.
+    representation = layers.Dropout(rate=dropout_rate)(representation)
+    # Compute logits outputs.
+    logits = layers.Dense(num_classes)(representation)
+    # Create the Keras model.
+    return keras.Model(inputs=inputs, outputs=logits)
+
+
+def run_experiment(model):
+    # Create Adam optimizer with weight decay.
+    optimizer = tf.keras.optimizers.AdamW(
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+    )
+    # Compile the model.
+    model.compile(
+        optimizer=optimizer,
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[
+            keras.metrics.SparseCategoricalAccuracy(name="acc"),
+            keras.metrics.SparseTopKCategoricalAccuracy(5, name="top5-acc"),
+        ],
+    )
+    # Create a learning rate scheduler callback.
+    reduce_lr = keras.callbacks.ReduceLROnPlateau(
+        monitor="val_loss", factor=0.5, patience=5
+    )
+    # Create an early stopping callback.
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience=10, restore_best_weights=True
+    )
+    # Fit the model.
+    history = model.fit(
+        x=x_train,
+        y=y_train,
+        batch_size=batch_size,
+        epochs=num_epochs,
+        validation_split=0.1,
+        callbacks=[early_stopping, reduce_lr],
+    )
+
+    _, accuracy, top_5_accuracy = model.evaluate(x_test, y_test)
+    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+    print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
+
+    # Return history to plot learning curves.
+    return history
+
+
+# Test the MLP Mixer Layer
+def test_mlp_mixer_layer():
+    mlpmixer_blocks = keras.Sequential(
+        [
+            MLPMixerLayer(num_patches, dropout_rate, embedding_dim)
+            for _ in range(num_blocks)
+        ]
+    )
+
+    mlpmixer_classifier = build_classifier(mlpmixer_blocks)
+    history = run_experiment(mlpmixer_classifier)
+    print(history.history)
