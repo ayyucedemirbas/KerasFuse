eb33c72092655b9e78f67e64cb4a3b87270cf1be
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-06-24 16:51:40 +0300

    ci: 🚧 pypi-publish action version changed to release/v1
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- kerasfuse/__init__.py
+++ kerasfuse/__init__.py
@@ -16,4 +16,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.1a0"
+__version__ = "0.10a1"
