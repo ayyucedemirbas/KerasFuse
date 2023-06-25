1ab4c44918dcf85fe8b877d9e4d292ecda30476b
Ayyuce Demirbas <a.ayyuced@gmail.com>
2023-06-25 13:54:16 +0300

    Create TransformerBlock.py

---
+++ kerasfuse/models/blocks/TransformerBlock.py
@@ -0,0 +1,27 @@
+from MultiHeadSelfAttention import MultiHeadSelfAttention
+
+from tensorflow.keras import layers
+import tensorflow as tf
+
+class TransformerBlock(layers.Layer):
+    def __init__(self, d_model, num_heads, dropout=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = MultiHeadSelfAttention(num_heads, d_model)
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        
+        self.ffn = tf.keras.Sequential([
+            layers.Dense(4*d_model, activation="relu"),
+            layers.Dense(d_model)
+        ])
+        self.dropout1 = layers.Dropout(dropout)
+        self.dropout2 = layers.Dropout(dropout)
+        
+    def call(self, inputs, training):
+        attn_output = self.att(inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
+     
